e9a8a00dcc34528feb89c7127294162b9e893231
Jing Zhang <jzhang_fl@outlook.com>
2024-10-31 10:38:39 -0500

    Input / Output added

--- SearchRange/SearchRange.cpp
+++ SearchRange/SearchRange.cpp
@@ -29,7 +29,24 @@ std::vector<int> SearchRange(std::vector<int>& arr, int key)
 
 int main()
 {
-    std::vector<int> vec{ 1, 2, 3, 3, 3, 4, 4, 5 };
-    std::vector<int> v = SearchRange(vec, 3);
-    std::cout << "The search range: {" << v[0] << ", " << v[1] << "}" << std::endl;
+    while (true)
+    {
+        int count = 0;
+        std::cout << "Number of elements in the array: ";
+        std::cin >> count;
+        if (count == 0)
+            break;
+
+        std::vector<int> vec(count);
+        std::cout << "The number array: ";
+        for (int i = 0; i < count; ++i)
+            std::cin >> vec[i];
+
+        int key = 0;
+        std::cout << "The search key: ";
+        std::cin >> key;
+
+        std::vector<int> v = SearchRange(vec, key);
+        std::cout << "The search range is: {" << v[0] << ", " << v[1] << "}" << std::endl;
+    }
 }
